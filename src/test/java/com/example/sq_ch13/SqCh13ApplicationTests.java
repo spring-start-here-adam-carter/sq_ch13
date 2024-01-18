25bb707f175cd45d6656448b5fd9442d714bcff7
Adam Carter <adam.clayton.carter@gmail.com>
2024-01-18 18:21:45 -0500

    Initial commit

---
+++ src/test/java/com/example/sq_ch13/SqCh13ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sq_ch13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqCh13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
